4543b3e0559d85efec985f89b74ba9dda1ce5a2f
Douglas Thomson <douglas.thomson@digital.cabinet-office.gov.uk>
2025-03-28 14:32:15 +0000

    Get tests working locally

--- backend/run-tests.sh
+++ backend/run-tests.sh
@@ -6,7 +6,7 @@ cd /backend
 mkdir -pv results
 
 if [[ "$TEST_ENVIRONMENT" == "dev" ]] || [[ "$TEST_ENVIRONMENT" == "build" ]]; then
-  if npm run test:api; then
+  if npm run test; then
     cp -rf results $TEST_REPORT_ABSOLUTE_DIR
   else
     cp -rf results $TEST_REPORT_ABSOLUTE_DIR
