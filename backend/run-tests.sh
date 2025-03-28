bb07259841c3b677216685d970911754031e0c0b
Douglas Thomson <douglas.thomson@digital.cabinet-office.gov.uk>
2025-03-28 14:29:41 +0000

    Aligning docker files with id-check async set up

--- backend/run-tests.sh
+++ backend/run-tests.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
+set -eu
 
-echo "Tests to be set up"
+cd /backend
+
+mkdir -pv results
+
+if [[ "$TEST_ENVIRONMENT" == "dev" ]] || [[ "$TEST_ENVIRONMENT" == "build" ]]; then
+  if npm run test:api; then
+    cp -rf results $TEST_REPORT_ABSOLUTE_DIR
+  else
+    cp -rf results $TEST_REPORT_ABSOLUTE_DIR
+    exit 1
+  fi
+fi
