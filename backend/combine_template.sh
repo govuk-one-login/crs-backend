20d8019cc8e5fe0c0a637cf5e11cff7e754aac40
Amrit Kandola <amrit.kandola@digital.cabinet-office.gov.uk>
2025-04-15 12:06:05 +0100

    DCMAW-11872 | add skeleton lambda

---
+++ backend/combine_template.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -eu
+
+if ! command -v rain &> /dev/null
+then
+    echo "Merging templates requires rain to be installed. See https://github.com/aws-cloudformation/rain for installation instructions."
+    exit 1
+fi
+
+echo $(find infra -type f -name "*.yaml" -print)
+rain merge $(find infra -type f -name "*.yaml" -print) -o template.yaml
