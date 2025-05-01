013a8369ab22a72a929531c582448d99ce59c7cb
Amrit Kandola <amrit.kandola@digital.cabinet-office.gov.uk>
2025-05-01 11:56:36 +0100

    added logger and metric filters

--- backend/src/functions/issueStatusListEntryHandler.ts
+++ backend/src/functions/issueStatusListEntryHandler.ts
@@ -1,6 +1,27 @@
-export function handler(): Response {
-  return new Response(null, {
-    status: 501,
-    statusText: "Not Implemented",
-  });
+import {APIGatewayProxyResult, APIGatewayProxyEvent, Context } from 'aws-lambda';
+import { logger } from '../common/logging/logger'
+import { LogMessage } from '../common/logging/LogMessages'
+
+
+export function handler(context: Context) {  
+  
+  setupLogger(context);
+  logger.info("Context" + context);
+  logger.info("version" + context.functionVersion);
+  logger.info(LogMessage.ISSUE_LAMBDA_STARTED);
+
+  return {
+    statusCode: 200,
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+      'idx': 3,
+      'uri': "https://douglast-backend.crs.dev.account.gov.uk/b/A671FED3E9AD" }),
+  };
 }
+
+function setupLogger(context: Context) {
+  logger.resetKeys();
+  logger.addContext(context);
+  logger.appendKeys({ functionVersion: context.functionVersion });
+}
+  
