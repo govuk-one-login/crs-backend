6533753c15fec32fd0f68150ca93143bf1ecfa4e
amrit-kandola <amrit.kandola@digital.cabinet-office.gov.uk>
2025-05-16 10:58:24 +0100

    setup error handling for the issuestatuslistentry lambda (#35)
    ## JIRA Ticket
    
    [DCMAW-12602](https://govukverify.atlassian.net/browse/DCMAW-12602)
    
    ## Description of Changes
    
    Added error handling and tests for the following scenarios :
    
    ClientId from clientRegistry doesnt match with the iss in the payload
    Bad data in payload (cant parse json)
    Not included issuer
    Not included expiry
    Not included kid
    Kid doesnt match the kid from the jwks uri on the matching clientId
    
    ## Evidence
    200: 
    <img width="698" alt="Screenshot 2025-05-15 at 15 00 41"
    src="https://github.com/user-attachments/assets/26784a7b-9acf-4aaa-87c9-530cbe0b11fa"
    />
    
    with following request:
    ```
    {
      "body": "eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6ImNjMmMzNzM4LTAzZWMtNDIxNC1hNjVlLTdmMDQ2MWEzNGU3YiJ9.eyJpc3MiOiJhc0tXbnNqZUVKRVdqandTSHNJa3NJa3NJaEJlIiwiZXhwaXJlcyI6IjE3MzQ3MDk0OTMifQ.OlAm7TIfn-Qrs2yJvl6MDr9raiq_uZ6FV7WwaPz2CTuCuK-EkvsqM8139yjIiJq3pqeZk0S_23J-4SGBAkUXhA",
      "path": "/issue",
      "httpMethod": "POST",
      "isBase64Encoded": true,
      "pathParameters": {
        "proxy": "/issue"
      },
      "headers": {
        "Accept": "application/json",
        "Host": "api.status-list.service.gov.uk"
      }
    }
    ```
    
    Example error - where theres a missing field in payload:
    <img width="500" alt="Screenshot 2025-05-15 at 15 02 13"
    src="https://github.com/user-attachments/assets/eab4fc9a-d14a-4f35-8336-a68007728efc"
    />
    
    
    ## Documentation
    
    <!-- 
    Links to documentation that has been created or updated to capture the
    changes.
    
    Major changes to the way the service is run and tested, or to the
    structure of this repository, should be
    captured in the README.
    
    Changes to API endpoints/contracts should be captured in the appropriate
    OpenAPI spec.
    
    Changes to the following should be captured in the appropriate Mobile
    Backend reference documentation on Confluence
    
    (https://govukverify.atlassian.net/wiki/spaces/DCMAW/pages/3861905465/Mobile+Backend+-+Reference):
      - Endpoints
      - Environment variables/configuration
      - Dependencies
    -->
    
    ---
    
    <details>
    <summary><h2>Guidance for Reviewers</h2></summary>
    
    ### Functionality
    * Have all of the ticket requirements and acceptance criteria been met?
    * Will this code function as expected in different environments?
    
    ### Testing
    
    * Is every component covered by high quality unit tests?
    * Have API/end-to-end tests been updated to cover key new behaviour?
    * Have infra tests been added if needed for changes to our templates?
    
    ### Logging
    
    * Are the changes supported by sufficient logging to help support, debug
    and analyse the behaviour of the service?
    * Is anything being logged that shouldn't be? (PII, redundant
    information)
    
    ### Code Quality
    
    * Does the code conform to our [values, principles and
    practices](https://govukverify.atlassian.net/wiki/spaces/DCMAW/pages/3910271319/Values+Principles+and+Practices)
    for high quality code?
    
    ### Documentation
    
    * Has any relevant reference documentation been updated?
    * Would any further documentation of the changes make the service easier
    to understand and maintain?
    
    </details>
    
    
    [DCMAW-12602]:
    https://govukverify.atlassian.net/browse/DCMAW-12602?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- backend/src/functions/issueStatusListEntryHandler.ts
+++ backend/src/functions/issueStatusListEntryHandler.ts
@@ -1,12 +1,102 @@
-import { APIGatewayProxyResult, Context } from "aws-lambda";
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  Context,
+} from "aws-lambda";
+import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
 import { logger } from "../common/logging/logger";
 import { LogMessage } from "../common/logging/LogMessages";
+import { decodeJwt, decodeProtectedHeader, JSONWebKeySet } from "jose";
+import { Readable } from "stream";
+import * as https from "node:https";
+
+// Define types for configuration
+interface StatusListEntry {
+  jwksUri: string;
+  type: string;
+  format: string;
+}
+
+interface ClientEntry {
+  clientName: string;
+  clientId: string;
+  statusList: StatusListEntry;
+}
+
+interface ClientRegistry {
+  clients: ClientEntry[];
+}
+
+const s3Client = new S3Client({});
+
+// S3 bucket and configuration file path
+const CONFIG_BUCKET = process.env.CLIENT_REGISTRY_BUCKET ?? "";
+const CONFIG_KEY =
+  process.env.CLIENT_REGISTRY_FILE_KEY ?? "mockClientRegistry.json";
 
 export async function handler(
+  event: APIGatewayProxyEvent,
   context: Context,
 ): Promise<APIGatewayProxyResult> {
   setupLogger(context);
   logger.info(LogMessage.ISSUE_LAMBDA_STARTED);
+
+  if (event.body == null) {
+    return badRequestResponse("No Request Body Found");
+  }
+
+  let jsonPayload;
+  let jsonHeader;
+
+  try {
+    // Parse the JWT without verifying the signature
+    const decodedPayload = decodeJwt(event.body);
+    const decodedHeader = decodeProtectedHeader(event.body);
+
+    const payloadString = JSON.stringify(decodedPayload);
+    const headerString = JSON.stringify(decodedHeader);
+    jsonPayload = JSON.parse(payloadString);
+    jsonHeader = JSON.parse(headerString);
+    logger.info("Succesfully decoded JWT as JSON");
+  } catch (error) {
+    logger.error("Error decoding or converting to JSON:", error);
+    return badRequestResponse("Error decoding JWT or converting to JSON");
+  }
+
+  if (!jsonPayload.iss) {
+    return badRequestResponse("No Issuer in Payload");
+  }
+  if (!jsonPayload.expires) {
+    return badRequestResponse("No Expiry Date in Payload");
+  }
+  if (!jsonHeader.kid) {
+    return badRequestResponse("No Kid in Header");
+  }
+
+  const config: ClientRegistry = await getConfiguration();
+
+  const matchingClientEntry = config.clients.find(
+    (i) => i.clientId == jsonPayload.iss,
+  );
+  if (!matchingClientEntry) {
+    return unauthorizedResponse(
+      "No matching client found with ID: " + jsonPayload.iss,
+    );
+  }
+  const jwksUri = matchingClientEntry.statusList.jwksUri;
+  if (!jwksUri) {
+    return internalServerErrorResponse(
+      "No jwksUri found on client ID: " + matchingClientEntry.clientId,
+    );
+  }
+  const jsonWebKeySet: JSONWebKeySet = await fetchJWKS(jwksUri);
+
+  const jwk = jsonWebKeySet.keys.find((key) => key.kid == jsonHeader.kid);
+  if (!jwk) {
+    return unauthorizedResponse(
+      "No matching Key ID found in JWKS Endpoint for Kid: " + jsonHeader.kid,
+    );
+  }
 
   return {
     statusCode: 200,
@@ -18,8 +108,112 @@ export async function handler(
   };
 }
 
+/**
+ * Helper function to fetch the JWKS from the URI
+ */
+async function fetchJWKS(jwksUri): Promise<JSONWebKeySet> {
+  return new Promise((resolve, reject) => {
+    const req = https.request(jwksUri, (res) => {
+      let data = "";
+
+      res.on("data", (chunk) => {
+        data += chunk;
+      });
+
+      res.on("end", () => {
+        try {
+          const jwks: JSONWebKeySet = JSON.parse(data);
+          resolve(jwks);
+        } catch (error) {
+          reject(new Error(`Failed to parse JWKS data: ${error.message}`));
+        }
+      });
+    });
+
+    req.on("error", (error) => {
+      reject(new Error(`Failed to fetch JWKS: ${error.message}`));
+    });
+
+    req.end();
+  });
+}
+
+/**
+ * Fetch the configuration from S3
+ */
+async function getConfiguration() {
+  logger.info("Fetching configuration from S3...");
+  logger.info(`Bucket: ${CONFIG_BUCKET}, Key: ${CONFIG_KEY}`);
+  try {
+    const command = new GetObjectCommand({
+      Bucket: CONFIG_BUCKET,
+      Key: CONFIG_KEY,
+    });
+
+    const response = await s3Client.send(command);
+    const bodyText = await streamToString(response.Body as Readable);
+    logger.info(`Fetched configuration: ${bodyText}`);
+    return JSON.parse(bodyText) as ClientRegistry;
+  } catch (error) {
+    logger.error("Error fetching configuration from S3:", error);
+    throw new Error("Error fetching configuration from S3");
+  }
+}
+
+/**
+ * Convert a readable stream to string
+ */
+async function streamToString(stream: Readable): Promise<string> {
+  logger.info("Converting stream to string...");
+  const chunks: Buffer[] = [];
+  for await (const chunk of stream) {
+    chunks.push(Buffer.from(chunk));
+  }
+  logger.info("Stream converted to string successfully");
+  return Buffer.concat(chunks).toString("utf-8");
+}
+
 function setupLogger(context: Context) {
   logger.resetKeys();
   logger.addContext(context);
   logger.appendKeys({ functionVersion: context.functionVersion });
 }
+
+const badRequestResponse = (
+  errorDescription: string,
+): APIGatewayProxyResult => {
+  return {
+    headers: { "Content-Type": "application/json" },
+    statusCode: 400,
+    body: JSON.stringify({
+      error: "BAD_REQUEST",
+      error_description: errorDescription,
+    }),
+  };
+};
+
+const unauthorizedResponse = (
+  errorDescription: string,
+): APIGatewayProxyResult => {
+  return {
+    headers: { "Content-Type": "application/json" },
+    statusCode: 401,
+    body: JSON.stringify({
+      error: "UNAUTHORISED",
+      error_description: errorDescription,
+    }),
+  };
+};
+
+const internalServerErrorResponse = (
+  errorDescription: string,
+): APIGatewayProxyResult => {
+  return {
+    headers: { "Content-Type": "application/json" },
+    statusCode: 500,
+    body: JSON.stringify({
+      error: "INTERNAL_SERVER_ERROR",
+      error_description: errorDescription,
+    }),
+  };
+};
