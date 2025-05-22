13bfd462e14c81500e6fd005dfeae7723c488888
ayeshakhan <ayesha.khan1@digital.cabinet-office.gov.uk>
2025-05-22 16:30:51 +0100

    DCMAW-11870: Added skeleton revoke lambda and supporting infrastructure

---
+++ backend/test/unit/revoke/revoke.test.ts
@@ -0,0 +1,10 @@
+import { handler } from "../../../src/functions/revokeHandler";
+import { describe, test, expect } from "@jest/globals";
+
+describe("testing handler setup correctly", () => {
+  test("handler should return appropriate response", () => {
+    expect(handler()).toBeInstanceOf(Response);
+    expect(handler().status).toBe(501);
+    expect(handler().statusText).toBe("Not Implemented");
+  });
+});
