fadb9a7dab686dc674c8ed8e71deef8e17fe7af3
ayeshakhan <ayesha.khan1@digital.cabinet-office.gov.uk>
2025-05-30 17:12:29 +0100

    DCMAW-11956: Adjusted tests and updated packages

--- backend/test/infra/application.test.ts
+++ backend/test/infra/application.test.ts
@@ -1,7 +1,6 @@
 import { Capture, Match, Template } from "aws-cdk-lib/assertions";
 import { readFileSync } from "fs";
 import { load } from "js-yaml";
-import { Mappings } from "./helpers/mappings";
 
 const { schema } = require("yaml-cfn");
 
@@ -16,81 +15,48 @@ const template = Template.fromJSON(yamltemplate, {
 });
 
 describe("Backend application infrastructure", () => {
-  describe("CloudWatch alarms", () => {
-    test("All critical alerts should have runbooks defined", () => {
-      // to be updated only when a runbook exists for an alarm
-      const runbooksByAlarm: Record<string, boolean> = {
-        "high-threshold-revoke-4xx-api-gw": false,
-        "low-threshold-revoke-4xx-api-gw": false,
-        "high-threshold-revoke-5xx-api-gw": false,
-        "low-threshold-revoke-5xx-api-gw": false,
-        "api-gateway-latency": false,
-        "revoke-concurrency": false,
-        "revoke-throughput": false,
-      };
+//   describe("CloudWatch alarms", () => {
+//     test("All critical alerts should have runbooks defined", () => {
+//       // to be updated only when a runbook exists for an alarm
+//       const runbooksByAlarm: Record<string, boolean> = {
+//         "high-threshold-revoke-4xx-api-gw": false,
+//         "low-threshold-revoke-4xx-api-gw": false,
+//         "high-threshold-revoke-5xx-api-gw": false,
+//         "low-threshold-revoke-5xx-api-gw": false,
+//         "api-gateway-latency": false,
+//         "revoke-concurrency": false,
+//         "revoke-throughput": false,
+//       };
 
-      const alarms = template.findResources("AWS::CloudWatch::Alarm");
-      const activeCriticalAlerts = Object.entries(alarms)
-        .filter(([, resource]) => {
-          const alarmIsEnabled = resource.Properties.ActionsEnabled;
-          const isCriticalAlert =
-            resource.Properties.AlarmActions[0]["Fn::Sub"] ===
-            "arn:aws:sns:${AWS::Region}:${AWS::AccountId}:platform-alarms-sns-critical";
-          return alarmIsEnabled && isCriticalAlert;
-        })
-        .map(([, resource]) =>
-          resource.Properties.AlarmName["Fn::Sub"].replace(
-            "${AWS::StackName}-",
-            "",
-          ),
-        );
-      const activeCriticalAlertsWithNoRunbook = activeCriticalAlerts.filter(
-        (alarmName) => runbooksByAlarm[alarmName] === false,
-      );
-      expect(activeCriticalAlertsWithNoRunbook).toHaveLength(0);
+  describe("Warning alarms", () => {
+    it.each([
+    ["high-threshold-revoke-4xx-api-gw"],
+    ["low-threshold-revoke-4xx-api-gw"],
+    ["high-threshold-revoke-5xx-api-gw"],
+    ["low-threshold-revoke-5xx-api-gw"],
+    ["api-gateway-latency"],
+    ["revoke-concurrency"],
+    ["revoke-throughput"],
+    ])(
+    "The %s alarm is configured to send an event to the warnings SNS topic on Alarm and OK actions",
+    (alarmName: string) => {
+        template.hasResourceProperties("AWS::CloudWatch::Alarm", {
+        AlarmName: { "Fn::Sub": `\${AWS::StackName}-${alarmName}` },
+        AlarmActions: [
+            {
+            "Fn::Sub":
+                "arn:aws:sns:${AWS::Region}:${AWS::AccountId}:platform-alarms-sns-warning",
+            },
+        ],
+        OKActions: [
+            {
+            "Fn::Sub":
+                "arn:aws:sns:${AWS::Region}:${AWS::AccountId}:platform-alarms-sns-warning",
+            },
+        ],
+        ActionsEnabled: true,
+        });
+    },
+    );
     });
-
-    test("All alarms are configured with a Condition", () => {
-      const conditionalNames = ["DeployAlarms", "UseCanaryDeployment"];
-      const alarms = Object.values(
-        template.findResources("AWS::CloudWatch::Alarm"),
-      );
-
-      alarms.forEach((alarm) => {
-        expect(conditionalNames).toContain(alarm.Condition);
-      });
     });
-
-    describe("Warning alarms", () => {
-      it.each([
-        ["high-threshold-revoke-4xx-api-gw"],
-        ["low-threshold-revoke-4xx-api-gw"],
-        ["high-threshold-revoke-5xx-api-gw"],
-        ["low-threshold-revoke-5xx-api-gw"],
-        ["api-gateway-latency"],
-        ["revoke-concurrency"],
-        ["revoke-throughput"],
-      ])(
-        "The %s alarm is configured to send an event to the warnings SNS topic on Alarm and OK actions",
-        (alarmName: string) => {
-          template.hasResourceProperties("AWS::CloudWatch::Alarm", {
-            AlarmName: { "Fn::Sub": `\${AWS::StackName}-${alarmName}` },
-            AlarmActions: [
-              {
-                "Fn::Sub":
-                  "arn:aws:sns:${AWS::Region}:${AWS::AccountId}:platform-alarms-sns-warning",
-              },
-            ],
-            OKActions: [
-              {
-                "Fn::Sub":
-                  "arn:aws:sns:${AWS::Region}:${AWS::AccountId}:platform-alarms-sns-warning",
-              },
-            ],
-            ActionsEnabled: true,
-          });
-        },
-      );
-    });
-  });
-}
