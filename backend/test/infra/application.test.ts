820087cb47377866c57d3b13e865d51cd2062125
ayeshakhan <ayesha.khan1@digital.cabinet-office.gov.uk>
2025-06-02 12:50:22 +0100

    DCMAW-11956: ran prettier formatting command with write permissions

--- backend/test/infra/application.test.ts
+++ backend/test/infra/application.test.ts
@@ -10,7 +10,7 @@ const yamltemplate: any = load(readFileSync("template.yaml", "utf-8"), {
 console.log("yamltemplate:" + yamltemplate);
 
 const template = Template.fromJSON(yamltemplate, {
-  skipCyclicalDependenciesCheck: true, 
+  skipCyclicalDependenciesCheck: true,
 });
 
 console.log("template:" + template);
@@ -18,33 +18,33 @@ console.log("template:" + template);
 describe("Backend application infrastructure", () => {
   describe("Warning alarms", () => {
     test.each([
-    ["revoke-concurrency"],
-    ["revoke-throughput"],
-    ["high-threshold-revoke-4xx-api-gw"],
-    ["low-threshold-revoke-4xx-api-gw"],
-    ["high-threshold-revoke-5xx-api-gw"],
-    ["low-threshold-revoke-5xx-api-gw"],
-    ["api-gateway-latency"],
+      ["revoke-concurrency"],
+      ["revoke-throughput"],
+      ["high-threshold-revoke-4xx-api-gw"],
+      ["low-threshold-revoke-4xx-api-gw"],
+      ["high-threshold-revoke-5xx-api-gw"],
+      ["low-threshold-revoke-5xx-api-gw"],
+      ["api-gateway-latency"],
     ])(
-    "The %s alarm is configured to send an event to the warnings SNS topic on Alarm and OK actions",
-    (alarmName: string) => {
+      "The %s alarm is configured to send an event to the warnings SNS topic on Alarm and OK actions",
+      (alarmName: string) => {
         template.hasResourceProperties("AWS::CloudWatch::Alarm", {
-        AlarmName: { "Fn::Sub": `\${AWS::StackName}-${alarmName}` },
-        AlarmActions: [
+          AlarmName: { "Fn::Sub": `\${AWS::StackName}-${alarmName}` },
+          AlarmActions: [
             {
-            "Fn::Sub":
+              "Fn::Sub":
                 "arn:aws:sns:${AWS::Region}:${AWS::AccountId}:platform-alarms-sns-warning",
             },
-        ],
-        OKActions: [
+          ],
+          OKActions: [
             {
-            "Fn::Sub":
+              "Fn::Sub":
                 "arn:aws:sns:${AWS::Region}:${AWS::AccountId}:platform-alarms-sns-warning",
             },
-        ],
-        ActionsEnabled: true,
+          ],
+          ActionsEnabled: true,
         });
-    },
+      },
     );
-    });
-    });
+  });
+});
