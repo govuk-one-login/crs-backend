ea0b149c2487cbb9029f894463e5d5a1501fcd8e
Matei Tanase <matei.tanase@digital.cabinet-office.gov.uk>
2025-06-26 13:39:19 +0100

    DCMAW-13991: Added iat to revoke request body

--- backend/src/functions/helper/jwtFunctions.ts
+++ backend/src/functions/helper/jwtFunctions.ts
@@ -84,6 +84,13 @@ export async function validateRevokingJWT(
   jsonHeader,
   config: ClientRegistry,
 ): Promise<ValidationResult> {
+  if (!jsonPayload.iat) {
+    return {
+      isValid: false,
+      error: badRequestResponse("Missing iat (Issued At) claim in JWT payload"),
+    };
+  }
+
   if (!jsonPayload.idx) {
     return {
       isValid: false,
