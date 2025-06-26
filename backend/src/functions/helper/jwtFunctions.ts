b7a5d6986bc6a1401a617b9370840dd337230626
Amrit Kandola <amrit.kandola@digital.cabinet-office.gov.uk>
2025-06-26 14:27:40 +0100

    Format

--- backend/src/functions/helper/jwtFunctions.ts
+++ backend/src/functions/helper/jwtFunctions.ts
@@ -80,7 +80,6 @@ export async function validateRevokingJWT(
   jsonHeader,
   config: ClientRegistry,
 ): Promise<ValidationResult> {
-
   if (!jsonPayload.iat) {
     return {
       isValid: false,
